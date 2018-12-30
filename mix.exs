abb853c1db7c8194cb0fdf1031d29c44a9e01056
nbitmage <ryota.kota@member.fsf.org>
2018-12-31 02:18:44 +0900

    configuration of escript-build

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config,
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
@@ -24,5 +25,9 @@ defmodule Issues.MixProject do
       { :httpoison, "~> 0.8" },
       { :poison, "~> 1.5" }
     ]
+  end
+
+  defp escript_config do
+    [main_module: Issues.CLI]
   end
 end
